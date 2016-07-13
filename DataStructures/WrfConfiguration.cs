61a68f478f42589235bb0ec2418645d8b2eb1cb0
Ben <ben@ben-rush.net>
2016-07-12 23:10:49 -0500

    Filesystem stuff is underway.

---
+++ DataStructures/WrfConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrfSharp.DataStructures
+{
+    public class WrfConfiguration
+    {
+        public string DataDirectory { get; set; }
+    }
+}
---
+++ WrfSharp.Helpers/FileSystem/FileSystemHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WrfSharp.DataStructures;
+
+namespace WrfSharp.Helpers.FileSystem
+{
+    public static class FileSystemHelper
+    {
+        public static void DeleteDataDirectory(WrfConfiguration config)
+        {
+            Directory.Delete(config.DataDirectory, true); 
+        }
+    }
+}
