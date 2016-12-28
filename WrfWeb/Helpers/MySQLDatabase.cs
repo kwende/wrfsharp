aaf85b4a35804556ad0bac2380ca398ac48d3c51
Ben <ben@ben-rush.net>
2016-12-27 23:11:43 -0600

    Removal of extra pages. Addition of mysql page. Getting ready to start showing stuff on website.

---
+++ WrfWeb/Helpers/MySQLDatabase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WrfWeb.Helpers
+{
+    public class MySQLDatabase
+    {
+        private string _connectionString; 
+
+        public MySQLDatabase(string connectionString)
+        {
+            _connectionString = connectionString; 
+        }
+
+        public void Connect()
+        {
+
+        }
+    }
+}
